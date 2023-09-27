8c2dc353eb908a14173decff6152d341b4df0b57
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2023-09-27 15:43:04 +0530

    Basic sveltekit webapp setup

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<h4>This app is build just to test sveltekit performance</h4>
+</div>
